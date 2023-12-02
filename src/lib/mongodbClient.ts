bab93786d05725c80f69ff8ec90ee7a0f079780a
jadeleonard <lukabartolomeo9@gmail.com>
2023-12-01 19:37:30 -0800

    added

--- src/lib/mongodbClient.ts
+++ src/lib/mongodbClient.ts
@@ -5,11 +5,11 @@ let isConnected = false;
 export const connectToDB = async () => {
   mongoose.set("strict", true);
 
-  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+  if (!process.env.MONGODB_URI) return console.log("MONGODB_URI not found");
   if (isConnected) return console.log("Already connected to MongoDB");
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log("Connected to MongoDB");
   } catch (error) {
